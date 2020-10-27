b8f6acc7e509c8089396a31bed4f673ad366638f
RES <info@neviplay.com>
2020-10-27 12:56:32 +0300

    Initial Commit

---
+++ ExchangeRate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExchangeRate
+//
+//  Created by Resul Emül on 10/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
